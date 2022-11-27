2d64cc1f5ccfb862e4235f66b642654914e137f7
Spythere <spythere@protonmail.com>
2022-11-27 15:36:50 +0100

    Workbox

--- vite.config.ts
+++ vite.config.ts
@@ -9,25 +9,25 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,png,svg,img}'],
-        runtimeCaching: [
-          {
-            urlPattern: new RegExp(`^https://spythere.github.io/api\/.*`),
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'github-api-cache',
-              expiration: {
-                maxEntries: 400,
-                maxAgeSeconds: 60 * 60 * 24 * 30, // <== 30 days
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-        ],
-      },
+      // workbox: {
+      //   globPatterns: ['**/*.{js,css,html,png,svg,img}'],
+      //   runtimeCaching: [
+      //     {
+      //       urlPattern: new RegExp(`^https://spythere.github.io/api\/.*`),
+      //       handler: 'NetworkFirst',
+      //       options: {
+      //         cacheName: 'github-api-cache',
+      //         expiration: {
+      //           maxEntries: 400,
+      //           maxAgeSeconds: 60 * 60 * 24 * 30, // <== 30 days
+      //         },
+      //         cacheableResponse: {
+      //           statuses: [0, 200],
+      //         },
+      //       },
+      //     },
+      //   ],
+      // },
       devOptions: {
         enabled: true,
       },
