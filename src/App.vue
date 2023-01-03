242be73c7894c318b3b0e42db0453229efa1e8a0
Spythere <spythere@protonmail.com>
2023-01-03 01:29:15 +0100

    pwa: automatyczny reload

--- src/App.vue
+++ src/App.vue
@@ -69,7 +69,9 @@ export default defineComponent({
   }),
 
   async created() {
-    const stockData = await (await fetch(`https://spythere.github.io/api/stockDataNew.json?t=${Math.floor(Date.now() / 60000)}`)).json();
+    const stockData = await (
+      await fetch(`https://spythere.github.io/api/stockDataNew.json?t=${Math.floor(Date.now() / 60000)}`)
+    ).json();
 
     this.store.stockData = stockData;
   },
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { registerSW } from 'virtual:pwa-register';
+
 import App from './App.vue';
 
 const pinia = createPinia();
 
-createApp(App)
-  .use(pinia)
-  .mount('#app');
+const updateSW = registerSW({
+  onOfflineReady() {},
+  
+  onNeedRefresh() {
+    console.log('Need refresh!');
+  },
+});
+
+createApp(App).use(pinia).mount('#app');
 
