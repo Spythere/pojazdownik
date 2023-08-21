4a1a840c16fa08f6b55c646f60a64fdb7b0d7058
Spythere <spythere@protonmail.com>
2023-08-22 00:52:51 +0200

    cleanup i18n; wykrywanie języka

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,10 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { registerSW } from 'virtual:pwa-register';
-import { createI18n } from 'vue-i18n';
 
 import App from './App.vue';
-import { LocaleMessageSchema, fetchBackendTranslations, locales } from './i18n';
-
+import i18n from './i18n-setup';
 const pinia = createPinia();
-
-const i18n = createI18n<[LocaleMessageSchema], 'en' | 'pl'>({
-  locale: 'en',
-  fallbackLocale: 'pl',
-  legacy: false,
-  globalInjection: true,
-  messages: locales,
-});
-
-fetchBackendTranslations(i18n);
 
 registerSW({
   immediate: true,
