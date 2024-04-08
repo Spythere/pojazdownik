6255efd98dfbc53da9b689eb32ed6819f3e2cd65
Spythere <spythere@protonmail.com>
2024-04-08 23:44:37 +0200

    fix: http url

--- src/http.ts
+++ src/http.ts
@@ -4,7 +4,7 @@ const http = axios.create({
   baseURL:
     import.meta.env.VITE_API_DEV === '1' && import.meta.env.DEV
       ? 'http://localhost:3001'
-      : 'https://spythere.spythere.eu',
+      : 'https://stacjownik.spythere.eu',
 });
 
 export default http;
