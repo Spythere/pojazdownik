99cbde382834f37b9a7b5b0535c2433640027e60
Spythere <spythere@protonmail.com>
2024-04-14 00:12:47 +0200

    chore: config port change

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 2137,
+    port: 2138,
   },
   plugins: [
     vue(),
