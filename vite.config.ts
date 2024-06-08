d726c8424e36c755d5d805675775de17ac86de7d
Spythere <spythere@protonmail.com>
2024-06-08 15:46:34 +0200

    chore: vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
       },
 
       workbox: {
+        cleanupOutdatedCaches: true,
         globPatterns: ['**/*.{js,css,html,jpg,png,svg,img,woff,woff2}'],
 
         runtimeCaching: [
